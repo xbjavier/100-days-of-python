8bf9e55cd15489f5178438f8a618c232c6edc6bb
rguajardo <xbeto@rguajardo.com>
2024-01-10 11:02:21 -0600

    Day 1

---
+++ Day01/BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name generator")
+city = input("Which city did you grew up in?\n")
+pet = input("What's the name of your pet?\n")
+print(f"{city} {pet}")
---
+++ Day01/PrintingToConsole.py
@@ -0,0 +1,16 @@
+# print to console
+print("hello world")
+
+#   string manipulation
+print("hello" + "world") 
+
+# print a number
+print(1)
+
+# Ask user for input + Python Variables
+name = input("What is your name? ")
+print("Hello " + name)
+
+# Calculate len of name
+name_lenght = len(name)
+print(name_lenght)
